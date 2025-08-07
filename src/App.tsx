07a8d6d797c5297d389e94dcee1c51a49fbdf169
GuiiHenriq <guilhermeerba@hotmail.com>
2025-08-07 20:32:21 -0300

    :wrench: chore: Add and configure TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+        <h1 className="text-4xl text-red-500">Timeline</h1>
+    </div>
+  )
+}
+
+export default App
